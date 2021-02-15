146b40292c318b93719cdc761a5bd5854c2c8dc1
Mike Shelby <mikegshelby@gmail.com>
2021-02-15 09:06:38 -0600

    added initial files and folders

---
+++ README.md
@@ -0,0 +1,51 @@
+# Pizza Hunt
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description and Purpose
+A web application for creating and commenting on pizzas!
+
+## Table of Contents
+  - [Description and Purpose](#description-and-purpose)
+  - [Application Screenshot](#application-screenshot)
+  - [Deployed Application](#deployed-application)
+  - [Built With](#built-with)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+## Application Screenshot
+![Pizza Hunt screenshot](./public/assets/images/pizza-hunt-screenshot.jpg "Application Screenshot")
+
+## Deployed Application
+https://pizza-hunt-0.herokuapp.com/
+
+## Built With
+* JavaScript
+* Node.js
+* Express.js
+* MongoDB
+* Mongoose
+* HTML
+* CSS
+* Heroku
+* MongoDB Atlas
+
+## License
+MIT License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Contributing
+* Mike Shelby
+
+## Tests
+All API end points were tested using Insomnia Core. Utility functions were tested using Jest.
+
+## Questions
+Github Username: mikegshelby
+
+Github Profile: https://github.com/mikegshelby
+
+Email: mikegshelby@gmail.com
+
+Please email me with any questions about this project!
---
+++ models/Comment.js
@@ -0,0 +1,70 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReplySchema = new Schema(
+  {
+    // set custom id to avoid confusion with parent comment's _id field
+    replyId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    replyBody: {
+      type: String,
+      required: 'You need to provide a reply to submit!',
+      trim: true
+    },
+    writtenBy: {
+      type: String,
+      required: 'You need to provide a name for your reply!',
+      trim: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+const CommentSchema = new Schema(
+  {
+    commentBody: {
+      type: String,
+      required: 'You need to provide a comment to submit!',
+      trim: true
+    },
+    writtenBy: {
+      type: String,
+      required: 'You need to provide a name for your comment!',
+      trim: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    // use ReplySchema to validate data for a reply
+    replies: [ReplySchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+const Comment = model('Comment', CommentSchema);
+
+// get total count of comments and replies on retrieval
+CommentSchema.virtual('replyCount').get(function() {
+  return this.replies.length;
+});
+
+module.exports = Comment;
